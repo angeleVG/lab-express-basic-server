1924a0cf45bcd467e416abd10fdcd3f563ab95b1
angeleVG <a.vangroesen@outlook.com>
2025-05-23 19:48:23 +0200

    create express server iteration 1

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
 
+const express = require("express");
+const morgan = require("morgan");
 
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
-
+const app = express();
 
 
 // MIDDLEWARE
 // Here you should set up the required middleware:
 // - `express.static()` to serve static files from the `public` folder
+app.use(express.static("public"));
 // - `express.json()` to parse incoming requests with JSON payloads
+app.use(express.json());
 // - `morgan` logger to log all incoming requests
-
+app.use(express.morgan("dev"));
 
 
 // ROUTES
@@ -23,3 +27,12 @@
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+
+// Define port number the server will listen on
+const PORT = 5005;
+
+// 1. Start the server and listen for incoming requests on the defined port
+// 2. Log a message to the console when the server is successfully running
+app.listen(PORT, () => {
+    console.log("Server is listening on port 5005");
+});
